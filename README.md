dc886500aacbf1c6bb8f7c9dc2a9b43ffe9cc779
Jules Damey <jfdamey@gmail.com>
2025-07-06 14:59:59 -0700

    added all files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# triple-ten-project
+# julestripleten
+#Project title : Vehicles Listings Dashboard
+#the app allows you check the conditions of various vehicles and their prices
+#this features scatter plot, histogram capability, and checkbox filters
+
+Installation:
+git clone https://github.com/jfdameyf/julestripleten.git
+cd tripleten
+python -m venv venv
+source venv/bin/activate
+pip isntall -r requirements.txt
---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+st.header("Vehicles Listings Dashboard")
+
+df = pd.read_csv('vehicles_us_data.csv')
+
+show_excellent = st.checkbox("Show only 'excellent' condition")
+show_good = st.checkbox("Show only 'good' condition")
+show_like_new = st.checkbox("Show only 'like new' condition")
+show_fair = st.checkbox("Show only 'fair' condition")
+if show_excellent:
+    data = df[df.condition == 'excellent']
+elif show_good:
+    data = df[df.condition == 'good']
+elif show_like_new:
+    data = df[df.condition == 'like new']
+else:
+    data = df[df.condition =='fair']
+
+hist = px.histogram(data, x = 'price', nbins=50, title='Price Distribution',
+                    labels= {'price': 'Listing Price (USD)'})
+
+st.plotly_chart(hist, use_container_width=True)
+
+scatter = px.scatter(data, x='odometer', y='price', color='fuel',
+                     opacity=0.5, title='Mileage vs. Price by fuel type',
+                     labels={'odomter':'Mileage (mi)', 'price': 'Price (USD)'})
+
+st.plotly_chart(scatter, use_container_width=True)
